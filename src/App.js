932a84e35541692cc5cb4454587fee475a5afa2e
ankit.bhatt <ankit.bhatt@sugarboxnetworks.com>
2020-10-04 13:57:34 +0530

    integrated redux

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React   from 'react';
 import classes from './App.module.css';
 import { Button, Container, Grid } from '@material-ui/core';
+import { connect } from "react-redux";
 
-function App() {
-  const [count, setCount] = useState(0);
 
-  const handleSub = () => {
-    setCount((prevState) => (prevState - 1))
-  }
+function App(props) {
+  // const [count, setCount] = useState(0);
 
-  const handleAdd = () => {
-    setCount((prevState) => (prevState + 1))
-  }
+  // const handleSub = () => {
+  //   setCount((prevState) => (prevState - 1))
+  // }
+
+  // const handleAdd = () => {
+  //   setCount((prevState) => (prevState + 1))
+  // }
 
   return (
     <Container maxWidth="xs" className={classes.container}>
       <div className={classes.count}>
-      <h4 style={{"textDecoration":"underline"}}>Redux Implementation</h4> 
-        Count : {count}
+        <h4 style={{ "textDecoration": "underline" }}>Redux Implementation</h4>
+        Count : {props.count}
       </div>
       <div className={classes.button}>
-        <Button variant="contained" color="secondary" onClick={() => handleSub()}>Subtract</Button>
-        <Button variant="contained" color="primary" onClick={() => handleAdd()}>Add</Button>
+        <Button variant="contained" color="secondary" onClick={props.decrement}>Subtract</Button>
+        <Button variant="contained" color="primary" onClick={props.increment} >Add</Button>
       </div>
     </Container>
   )
+
 }
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    count: state
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch({ type: 'INCREMENT' }),
+    decrement: () => dispatch({ type: 'DECREMENT' })
+  };
+
+}
+export default connect(mapStateToProps,mapDispatchToProps)(App);
